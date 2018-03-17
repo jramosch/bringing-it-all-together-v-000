68de7f0873045139338bde1ddbaefb6a292f7011
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-17 18:43:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,6 +61,11 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
     dog = DB[:conn].execute(sql, name, breed)
+    if !dog.empty?
+      self.create(name: name, breed: breed)
+    else
+      
+    end
   end
 
   def update
