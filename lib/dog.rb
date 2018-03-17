71de96c2ed99c5bd2166310f95308469986b0e59
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-17 18:42:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,7 +59,8 @@ class Dog
   end
 
   def self.find_or_create_by(name:, breed:)
-    
+    sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
+    dog = DB[:conn].execute(sql, name, breed)
   end
 
   def update
