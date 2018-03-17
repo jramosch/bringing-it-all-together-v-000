07c16d2977394852ef54850e4ecca22ce732483a
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-17 18:52:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,6 +73,10 @@ class Dog
     dog
   end
 
+  def self.find_by_name(name)
+    
+  end
+
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
