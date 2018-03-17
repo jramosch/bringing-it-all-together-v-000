dd27934bcaeee9d9971164c08b9fd599196549fb
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-17 18:38:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,18 +44,18 @@ class Dog
     dog
   end
 
-  def self.find_by_id(id)
-    sql = "SELECT * FROM dogs WHERE id = ?"
-    row = DB[:conn].execute(sql, id)
-    self.new_from_db(row.flatten)
-  end
-
   def self.new_from_db(row)
     id = row.flatten[0]
     name = row.flatten[1]
     breed = row.flatten[2]
 
     Dog.new(id: id, name: name, breed: breed)
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    row = DB[:conn].execute(sql, id)
+    self.new_from_db(row.flatten)
   end
 
   def update
